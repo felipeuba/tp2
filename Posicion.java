71c9a8debfca627513bec05db97b4d49d0e62e31
felipeuba <165933197+felipeuba@users.noreply.github.com>
2024-11-11 20:11:47 -0300

    Delete Posicion.java

--- Posicion.java
+++
@@ -1,8 +0,0 @@
-package aed;
-
-public interface Posicion {
-    void setPosicionGanancia(int posicion);
-    void setPosicionTimestamp(int posicion);
-    void setPosicionCiudad(int posicion);
-}
-
