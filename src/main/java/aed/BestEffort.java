7caeca9fa0d82b12a7fbace8773f4c1a4a4da0be
ToniBidart <165932496+ToniBidart@users.noreply.github.com>
2024-11-12 14:24:01 -0300

    Update BestEffort.java

--- src/main/java/aed/BestEffort.java
+++ src/main/java/aed/BestEffort.java
@@ -1,13 +1,40 @@
 package aed;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class BestEffort {
-    //Completar atributos privados
+    private Heap<Ciudad> mayorSuperavit;
+    private ArrayList<Ciudad> mayorGanancia;
+    private ArrayList<Ciudad> mayorPerdida;
+    private ArrayList<Ciudad> ciudades;
+    private Heap<Traslado> trasladosMasAntiguos;
+    private Heap<Traslado> trasladosMasRedituables;
+// NO PODEMOS USAR TUPLAS    private ##### gananciaTotal;
 
-    public BestEffort(int cantCiudades, Traslado[] traslados){
-        // Implementar
+    public BestEffort(int cantCiudades, Traslado[] traslados) {
+        ciudades = new Ciudad[cantCiudades];
+        for (int i = 0; i < cantCiudades; i++) {
+            ciudades[i] = new Ciudad();
+        }
+        mayorGanancia  = new ArrayList<>(cantCiudades);
+        mayorPerdida = new ArrayList<>(cantCiudades);
+        //esto evita el aliassing
+        for (Ciudad ciudad : ciudades) {
+            mayorGanancia.add(ciudad);
+            mayorPerdida.add(ciudad);
+        }
+        gananciaTotal = (0,0);
+
+
+//FELI, ESTÁ BIEN?
+
+        mayorSuperavit = new Heap<>(new ArrayList<>(List.of(ciudades)), CustomComparator.BY_BALANCE);
+        trasladosMasAntiguos = new Heap<>(new ArrayList<>(List.of(traslados)), CustomComparator.BY_TIMESTAMP);
+        trasladosMasRedituables = new Heap<>(new ArrayList<>(List.of(traslados)), CustomComparator.BY_GANANCIA);
     }
+
+
 
     public void registrarTraslados(Traslado[] traslados){
         // Implementar
