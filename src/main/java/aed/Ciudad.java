d8e9b08b52063c6a865f9e38d63ca761d506d174
ToniBidart <165932496+ToniBidart@users.noreply.github.com>
2024-11-12 15:37:19 -0300

    Toni

--- src/main/java/aed/Ciudad.java
+++ src/main/java/aed/Ciudad.java
@@ -7,18 +7,37 @@ public class Ciudad{
     private int id;
     private int posicionHeap;
 
-    public Ciudad(){
+    public Ciudad(int i){
         this.ganancia = 0;
         this.perdida = 0;
         this.balance = 0;
+        this.id = i;
     }
     public int getBalance(){
         return this.balance;
+    }
+    public int getPerdida(){
+        return this.perdida;
+    }
+    public int getGanancia(){
+        return this.ganancia;
     }
     public int getId(){
         return this.id;
     }
     public void setPosicionCiudad(int i){
         this.posicionHeap = i;
+    }
+    public void setGanancia(int n){
+        this.ganancia += n;
+    }
+    public void setPerdida(int n){
+        this.perdida += n;
+    }
+    public void sumarBalance(int n){
+        this.balance += n;
+    }
+    public void restarBalance(int n){
+        this.balance -= n;
     }
 }
