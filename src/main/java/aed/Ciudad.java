763f6b8e89568bffcf888160f9e31ac1a1e3b96c
felipeuba <165933197+felipeuba@users.noreply.github.com>
2024-11-12 16:19:54 -0300

    agregamos un getter

--- src/main/java/aed/Ciudad.java
+++ src/main/java/aed/Ciudad.java
@@ -7,16 +7,20 @@ public class Ciudad{
     private int id;
     private int posicionHeap;
 
-    public Ciudad(){
+    public Ciudad(int i){
         this.ganancia = 0;
         this.perdida = 0;
         this.balance = 0;
+        this.id = i;
     }
     public int getBalance(){
         return this.balance;
     }
     public int getId(){
         return this.id;
+    }
+    public int getPosicionHeap(){
+        return this.posicionHeap;
     }
     public void setPosicionCiudad(int i){
         this.posicionHeap = i;
